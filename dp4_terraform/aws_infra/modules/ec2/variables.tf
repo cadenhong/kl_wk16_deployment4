25d0a06a4dde33d9516fcdf40cc7cdda4f23ebc3
Caden Hong <caden.p.hong@gmail.com>
2022-10-29 02:06:05 -0400

    rearranged repo

--- dp4_terraform/aws_infra/modules/ec2/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "vpc_id" {}
-variable "ec2-properties" {}
-variable "subnet1_id" {}
-variable "subnet2_id" {}
--- dp4_terraform/aws_infra/modules/vpc/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "vpc-cidr" {}
-variable "sub1-cidr" {}
-variable "sub2-cidr" {}
